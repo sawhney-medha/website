8a453a92d5f338b9f17fd568e8031759cf777663
sawhney-medha <sawhney.medha@gmail.com>
2025-01-24 05:15:31 +0000

    Deploying to gh-pages from @ sawhney-medha/website@a76129b398ed966857bae98d20f3d93d5df4a317 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/CV/";
           },
         },{id: "post-a-post-with-image-galleries",
       
